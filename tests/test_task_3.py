a6d849f16f232b81a34e376d6b2e672d8f2e4c35
maphane123a <maphane123a@student.wethinkcode.co.za>
2024-02-19 15:27:14 +0000

    added unittest

---
+++ tests/test_task_3.py
@@ -0,0 +1,73 @@
+'''
+   Mpho Aphane                          -0~0-
+   mpho4phane@gmail.com                  ._.
+'''
+
+import unittest
+from io import StringIO
+from unittest.mock import patch
+import task_3.password_generator as password_generator
+
+class TestTask3(unittest.TestCase):
+    
+    # Testing the get_password_length function which prompts the user to input the length of the password
+    # by mocking the input using patch when calling the function, the return
+    # value must be the same as the input value of String type.
+    @patch('sys.stdin', StringIO('Mpho\n9\n1 up\ntail\nCool\nJuiCe\n'))
+    def test_get_password_length(self):
+        return_value = password_generator.get_password_length()
+        self.assertTrue(isinstance(return_value, str))
+        self.assertEqual(return_value, 'Mpho')
+        
+        return_value = password_generator.get_password_length()
+        self.assertTrue(isinstance(return_value, str))
+        self.assertEqual(return_value, '9')
+        
+        return_value = password_generator.get_password_length()
+        self.assertTrue(isinstance(return_value, str))
+        self.assertEqual(return_value, '1 up')
+        
+    
+    # The validate_length function calls the get_input function if an invalid input was provided.
+    # This annotation serves to mock valid input after testing the validate_length function with an invalid input.
+    @patch('sys.stdin', StringIO('9\n16\n'))
+    def test_validate_length(self):
+        # Test the validate_length function with an invalid password length of '1' which is not in the required range.
+        return_value = password_generator.validate_length('1')
+        
+        # The patch annotation provides a valid password length of 9 which gets returned as an integer.
+        self.assertTrue(isinstance(return_value, int))
+        self.assertEqual(return_value, 9)
+        
+        
+        # Test the validate_length function with an invalid password length of 'Python'.
+        return_value = password_generator.validate_length('Python')
+        
+        # The patch annotation provides a valid password length of 16 which gets returned as an integer.
+        self.assertTrue(isinstance(return_value, int))
+        self.assertEqual(return_value, 16)
+        
+        
+        # Test the validate_length function with a valid password length of '20' which should be returned as an int
+        # Mocking is not required since the value of the parameter is in range.
+        return_value = password_generator.validate_length('20')
+        self.assertTrue(isinstance(return_value, int))
+    
+    
+    def test_generate_password(self):
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', 
+                     '_', '=', '+', '\\', '/', ',', '.', '?', '<', '>', '`',
+                    '~', '[', ']', '{', '}', '"', '\'']
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        
+        # Test whether the generated password has a small-case and upper-case letter with a number and a special character.
+        result = password_generator.generate_password(9)
+        self.assertTrue([result_char in special_chars for result_char in result])
+        self.assertTrue([result_char in numbers for result_char in result])
+        self.assertTrue([result_char in letters for result_char in result])
+        
+    
+if __name__ == '__main__':
+    unittest.main()
